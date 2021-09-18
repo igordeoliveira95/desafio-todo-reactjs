cadfa3bcfb85601ff050beb0246690f4c545123b
Igor de Oliveira <igordeoliveira2212@gmail.com>
2021-09-17 22:04:24 -0300

    TODO completa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -6,24 +6,45 @@ import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
 interface Task {
   id: number;
-  title: string;
+  taskDescription: string;
   isComplete: boolean;
 }
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [taskInput, setTaskInput] = useState('');
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!taskInput || taskInput === ' ') {
+      alert("Não é possível adicionar uma task vázia.");
+      return;
+    };
+    
+    const newTask: Task = {
+      id: Math.random(),
+      taskDescription: taskInput,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setTaskInput('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const newTasks = tasks.map( task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
@@ -35,8 +56,8 @@ export function TaskList() {
           <input 
             type="text" 
             placeholder="Adicionar novo todo" 
-            onChange={(e) => setNewTaskTitle(e.target.value)}
-            value={newTaskTitle}
+            onChange={(e) => setTaskInput(e.target.value)}
+            value={taskInput}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             <FiCheckSquare size={16} color="#fff"/>
@@ -58,7 +79,7 @@ export function TaskList() {
                   />
                   <span className="checkmark"></span>
                 </label>
-                <p>{task.title}</p>
+                <p id="taskDescription" contentEditable="true">{task.taskDescription}</p>
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
@@ -68,6 +89,7 @@ export function TaskList() {
           ))}
           
         </ul>
+        <p className="editMessage">* Você pode editar a task clicando nela :D</p>
       </main>
     </section>
   )
